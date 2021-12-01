eaf29872cbc25ec6121f9ebb4eb4a9049b82dc1d
SChernykh <sergey.v.chernykh@gmail.com>
2021-12-01 18:24:36 +0100

    Update version

--- src/util.cpp
+++ src/util.cpp
@@ -32,7 +32,7 @@ namespace p2pool {
 #define STR2(X) STR(X)
 #define STR(X) #X
 
-const char* VERSION = "v1.3.1 (built"
+const char* VERSION = "v1.4.0 (built"
 #if defined(__clang__)
 	" with clang/" __clang_version__
 #elif defined(__GNUC__)
