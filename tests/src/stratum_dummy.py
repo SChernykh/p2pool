d08ac60fd91eb6defeaf319970d0a3e5483f5e08
SChernykh <sergey.v.chernykh@gmail.com>
2023-05-25 09:44:44 +0200

    CI: set difficulties for fake stratum clients
    Client 1: autodiff
    Client 2: fixed 1000 diff
    Client 3: fixed 10M diff

--- tests/src/stratum_dummy.py
+++ tests/src/stratum_dummy.py
@@ -2,7 +2,7 @@ import socket
 import time
 import sys
 
-f = open(sys.argv[1], 'wb');
+f = open('stratum_dummy' + sys.argv[1] + '.log', 'wb');
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.settimeout(1)
@@ -15,7 +15,15 @@ while sock.connect_ex(('127.0.0.1', 3333)) != 0:
 	print('.')
 	time.sleep(1)
 
-sock.sendall(b'{"id":1,"method":"login","params":{"login":"x"}}\n')
+diff = '';
+
+if (sys.argv[1] == '2'):
+	diff = '+1000';
+if (sys.argv[1] == '3'):
+	diff = '+10000000';
+
+request = '{"id":1,"method":"login","params":{"login":"x' + diff + '"}}\n'
+sock.sendall(request.encode('utf-8'))
 
 while True:
 	data = sock.recv(1024)
