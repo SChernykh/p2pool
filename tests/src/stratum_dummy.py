513c2dfc3b3b76f0eca6d3fae814f82f1528ab52
SChernykh <sergey.v.chernykh@gmail.com>
2023-09-16 22:29:32 +0200

    Updated copyright

--- tests/src/stratum_dummy.py
+++ tests/src/stratum_dummy.py
@@ -1,3 +1,18 @@
+# This file is part of the Monero P2Pool <https://github.com/SChernykh/p2pool>
+# Copyright (c) 2021-2023 SChernykh <https://github.com/SChernykh>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 import socket
 import time
 import sys
