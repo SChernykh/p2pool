e44df03d8277a7fab934e0f921ce91fc761f9cc7
SChernykh <15806605+SChernykh@users.noreply.github.com>
2023-12-20 17:30:35 +0100

    Fixed cppcheck test

--- cppcheck/run.cmd
+++ cppcheck/run.cmd
@@ -1,6 +1,7 @@
 @echo off
 "..\cppcheck-main\bin\cppcheck.exe" --project=..\build\p2pool.vcxproj --project-configuration="Release|x64" -DSIZE_MAX=UINT64_MAX -DRAPIDJSON_ENDIAN=RAPIDJSON_LITTLEENDIAN --platform=win64 --std=c++14 --enable=all --inconclusive --inline-suppr --template="{file}:{line}:{id}{inconclusive: INCONCLUSIVE} {message}" --suppressions-list=suppressions.txt --output-file=errors_full.txt --max-ctu-depth=3 --check-level=exhaustive
-findstr /V /C:"external\src" errors_full.txt > errors_filtered.txt
+findstr /V /C:"external\src" errors_full.txt > errors_filtered0.txt
+findstr /V /C:":checkersReport" errors_filtered0.txt > errors_filtered.txt
 for /f %%i in ("errors_filtered.txt") do set size=%%~zi
 if %size% gtr 0 (
 	type errors_filtered.txt
--- cppcheck/run.sh
+++ cppcheck/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ../cppcheck-main/cppcheck --project=../build/compile_commands.json -DSIZE_MAX=UINT64_MAX -DRAPIDJSON_ENDIAN=RAPIDJSON_LITTLEENDIAN -D__SSE2__=1 --platform=unix64 --std=c++14 --enable=all --inconclusive --inline-suppr --template="{file}:{line}:{id}{inconclusive: INCONCLUSIVE} {message}" --suppressions-list=suppressions.txt --output-file=errors_full.txt --max-ctu-depth=3 --check-level=exhaustive
-grep -v 'external' errors_full.txt > errors_filtered.txt
+grep -v 'external' errors_full.txt > errors_filtered0.txt
+grep -v ':checkersReport' errors_filtered0.txt > errors_filtered.txt
 if [ -s errors_filtered.txt ]; then
 	cat errors_filtered.txt
 	exit 1
