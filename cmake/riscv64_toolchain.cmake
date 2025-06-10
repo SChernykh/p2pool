8bb317c4b7d8a4f5bdbde81be4d25872d8c72b30
SChernykh <15806605+SChernykh@users.noreply.github.com>
2025-06-10 19:13:14 +0200

    CI: added Ubuntu RISC-V build, fixed Windows builds

---
+++ cmake/riscv64_toolchain.cmake
@@ -0,0 +1,8 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_CROSSCOMPILING TRUE)
+
+set(CMAKE_SYSTEM_PROCESSOR riscv64)
+
+set(CMAKE_C_COMPILER /usr/bin/riscv64-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER /usr/bin/riscv64-linux-gnu-g++)
+set(CMAKE_STRIP /usr/bin/riscv64-linux-gnu-strip)
