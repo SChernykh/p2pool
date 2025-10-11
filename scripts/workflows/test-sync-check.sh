977cd73bbd049d7c452e9658d12e6b4ec82a70d7
SChernykh <15806605+SChernykh@users.noreply.github.com>
2025-10-11 16:31:21 +0200

    CI: updated sync test's error check

--- scripts/workflows/test-sync-check.sh
+++ scripts/workflows/test-sync-check.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-grep 'ERROR ' p2pool.log | grep -E -v 'submit_block|failed to bind' > errors.log
+grep 'ERROR ' p2pool.log | grep -E -v 'submit_block|failed to bind|ZMQ is not running' > errors.log
 
 if [ -s errors.log ]; then
+	cat errors.log
 	exit 1
 fi
