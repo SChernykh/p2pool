d8662ad41e4208dc492f4ea720d4a6144aef81e9
SChernykh <15806605+SChernykh@users.noreply.github.com>
2025-10-25 17:32:12 +0200

    Updated build scripts

--- cmake/macos_x86_64_toolchain_clang.cmake
+++ cmake/macos_x86_64_toolchain_clang.cmake
@@ -3,7 +3,7 @@ set(CMAKE_CROSSCOMPILING TRUE)
 
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
 
-set(CMAKE_C_COMPILER /usr/local/bin/x86_64-apple-darwin24.5-clang)
-set(CMAKE_CXX_COMPILER /usr/local/bin/x86_64-apple-darwin24.5-clang++)
-set(CMAKE_ASM_COMPILER /usr/local/bin/x86_64-apple-darwin24.5-as)
-set(CMAKE_STRIP /usr/local/bin/x86_64-apple-darwin24.5-strip)
+set(CMAKE_C_COMPILER /usr/local/bin/x86_64-apple-darwin25-clang)
+set(CMAKE_CXX_COMPILER /usr/local/bin/x86_64-apple-darwin25-clang++)
+set(CMAKE_ASM_COMPILER /usr/local/bin/x86_64-apple-darwin25-as)
+set(CMAKE_STRIP /usr/local/bin/x86_64-apple-darwin25-strip)
