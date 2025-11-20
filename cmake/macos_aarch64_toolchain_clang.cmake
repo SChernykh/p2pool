b81a582154b43ff57723884fd221c14001cf54cc
SChernykh <15806605+SChernykh@users.noreply.github.com>
2025-11-20 14:30:02 +0100

    Build scripts: updated versions

--- cmake/macos_aarch64_toolchain_clang.cmake
+++ cmake/macos_aarch64_toolchain_clang.cmake
@@ -3,7 +3,7 @@ set(CMAKE_CROSSCOMPILING TRUE)
 
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
-set(CMAKE_C_COMPILER /usr/local/bin/aarch64-apple-darwin25-clang)
-set(CMAKE_CXX_COMPILER /usr/local/bin/aarch64-apple-darwin25-clang++)
-set(CMAKE_ASM_COMPILER /usr/local/bin/aarch64-apple-darwin25-as)
-set(CMAKE_STRIP /usr/local/bin/aarch64-apple-darwin25-strip)
+set(CMAKE_C_COMPILER /usr/local/bin/aarch64-apple-darwin25.1-clang)
+set(CMAKE_CXX_COMPILER /usr/local/bin/aarch64-apple-darwin25.1-clang++)
+set(CMAKE_ASM_COMPILER /usr/local/bin/aarch64-apple-darwin25.1-as)
+set(CMAKE_STRIP /usr/local/bin/aarch64-apple-darwin25.1-strip)
