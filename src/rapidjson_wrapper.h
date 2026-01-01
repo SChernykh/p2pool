313fb64b4125e6e7681bde17124be446e39c3195
SChernykh <15806605+SChernykh@users.noreply.github.com>
2026-01-01 22:13:05 +0100

    Updated copyright

--- src/rapidjson_wrapper.h
+++ src/rapidjson_wrapper.h
@@ -1,3 +1,20 @@
+/*
+ * This file is part of the Monero P2Pool <https://github.com/SChernykh/p2pool>
+ * Copyright (c) 2021-2026 SChernykh <https://github.com/SChernykh>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #if defined(_MSC_VER) && defined(__cppcheck__)
 // cppcheck chokes on rapidjson headers in Windows
 #else
